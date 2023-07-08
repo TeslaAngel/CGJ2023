d463c610eb3b5f3ce67c6e7ef003c0ce8f9c5c41
TeslaAngel <3363154242@qq.com>
2023-07-08 14:44:32 +0800

    initing camera chase

--- TouchThePath/Assets/Scripts/PlayerScript.cs
+++ TouchThePath/Assets/Scripts/PlayerScript.cs
@@ -14,20 +14,6 @@ public class PlayerScript : MonoBehaviour
     internal float HandPrintLoadtime = 0f;
     public float HandPrintLimit;
     public float HandPrintInterval;
-    
-
-    void ShootHandPrint()
-    {
-        Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 1);
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-
-        if (hit.collider != null)
-        {
-            //Initiate a handprint on wall
-            Instantiate(Prefab_HandPrint, hit.point, Quaternion.Euler(0f, 0f, angle));//TO BE ALTERED
-        }
-    }
 
     void Awake()
     {
@@ -47,13 +33,28 @@ public class PlayerScript : MonoBehaviour
         //Behavior: handPrint
         if(Input.GetAxis("Fire1") > 0 && HandPrintLoadtime <= 0f && Prefab_HandPrint.GetComponent<SpriteRenderer>().color.a > 0)
         {
+            //raycast
+            Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 1);
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            if (hit.collider != null)
+            {
+                //Initiate a handprint on wall
+                Instantiate(Prefab_HandPrint, hit.point, Quaternion.Euler(0f, 0f, angle));//TO BE ALTERED
+            }
+            else
+            {
+                return;
+            }
+
             //alter alpha value
             Color OriginalCol = Prefab_HandPrint.GetComponent<SpriteRenderer>().color;
-            ShootHandPrint();
             Prefab_HandPrint.GetComponent<SpriteRenderer>().color = new Color(OriginalCol.r, OriginalCol.g, OriginalCol.b, OriginalCol.a - (1f/HandPrintLimit));
             HandPrintLoadtime = HandPrintInterval;
         }
-        else if(HandPrintLoadtime > 0f)
+
+        //timer for handprint
+        if(HandPrintLoadtime > 0f)
         {
             HandPrintLoadtime -= Time.deltaTime;
         }
