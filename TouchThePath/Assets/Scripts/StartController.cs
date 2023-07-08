cff1f27b6aafcb053cad69efb6750f921787d539
LiChong <qq2012110121@163.com>
2023-07-08 14:19:28 +0800

    添加开始场景(start)和关卡选择场景(level_select); 导入TMP资源.

---
+++ TouchThePath/Assets/Scripts/StartController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class StartController : MonoBehaviour
+{
+	public Button startButton;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		CursorHelper.SetImage(CursorImageType.PalmNormal);
+		startButton.onClick.AddListener(OnClickStart);
+	}
+
+
+	void OnClickStart()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("level_select");
+	}
+
+
+}
